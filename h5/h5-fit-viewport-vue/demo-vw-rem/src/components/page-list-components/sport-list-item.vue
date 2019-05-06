673e4b6d25bc160b2d532983b5067a8b71525025
Tycalwayslove <1186446929@qq.com>
2019-05-06 23:13:43 +0800

    <fix>整理下今天的东西，明天解决编译scss的问题

---
+++ h5/h5-fit-viewport-vue/demo-vw-rem/src/components/page-list-components/sport-list-item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="sport-list">
+    <div class="sport-list-el">
+      <ul class="game-list">
+        <li class="group-info">05-06</li>
+        <li class="game-item">
+          <a href="#" class="detail-url">
+            <div class="game-desc">英超第三轮</div>
+            <div class="game-info">
+              <div class="left">
+                <img src="http://mat1.gtimg.com/sports/logo/yingchao/afd.png" alt>
+                <h3>埃弗顿</h3>
+              </div>
+              <div class="right">
+                <img src="http://mat1.gtimg.com/sports/logo/yingchao/afd.png" alt>
+                <h3>埃弗顿</h3>
+              </div>
+              <div class="game-status">
+                <div class="goals">2:0</div>
+                <div class="game-icon">
+                  <i></i>
+                  集锦
+                </div>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.game-list .group-info {
+  height: 25px;
+  padding-left: 10px;
+  font-size: 12px;
+  line-height: 25px;
+  color: #666;
+  background-color: #ebf0f5;
+}
+</style>
--- h5/h5-fit-viewport-vue/demo-vw-rem/src/views/GameList.vue
+++ h5/h5-fit-viewport-vue/demo-vw-rem/src/views/GameList.vue
@@ -1,15 +1,16 @@
 <!-- 游戏列表 -->
 <template>
   <div class="className">
-
+    <sport-list-item></sport-list-item>
   </div>
 </template>
 
 <script>
+import sportListItem from "@/components/page-list-components/sport-list-item";
 import { getGameList, test } from "@/api/index";
 export default {
   name: "gamelist",
-  components: {},
+  components: { sportListItem },
 
   data() {
     return {};
