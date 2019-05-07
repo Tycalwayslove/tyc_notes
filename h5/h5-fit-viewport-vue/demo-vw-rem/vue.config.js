71c797142e96f3d84ef1cf9fe7fab26c8bb84c0b
Tycalwayslove <1186446929@qq.com>
2019-05-07 10:06:53 +0800

    <feat> 引用sass-source-loader 全局使用scss文件。

---
+++ h5/h5-fit-viewport-vue/demo-vw-rem/vue.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const process = require("process");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: "/",
+  outputDir: resolve("/m"),
+  assetsDir: "static",
+  devServer: {
+    open: true,
+    proxy: null,
+    disableHostCheck: true
+  },
+  productionSourceMap: false, // 生产环境禁用
+
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule("scss").oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item
+        .use("sass-resources-loader")
+        .loader("sass-resources-loader")
+        .options({
+          // Provide path to the file with resources
+          resources: [
+            "./src/assets/css/variable.scss",
+            "./src/assets/css/utils.scss"
+          ]
+        })
+        .end();
+    });
+    config.resolve.alias
+      .set("@common", resolve("src/common"))
+      .set("@store", resolve("src/store"))
+      .set("@components", resolve("src/components"))
+      .set("@mixins", resolve("src/mixins"))
+      .set("@api", resolve("src/api"))
+      .set("@directives", resolve("src/directives"))
+      .set("@pages", resolve("src/pages"));
+  }
+};
